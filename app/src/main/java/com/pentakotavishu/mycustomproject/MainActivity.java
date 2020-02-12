b677d0b47465150976f88a5d859734c6eb9d811d
vishpentak <vishpentak@gmail.com>
2020-02-12 01:19:45 -0500

    Initial commit

---
+++ app/src/main/java/com/pentakotavishu/mycustomproject/MainActivity.java
@@ -0,0 +1,18 @@
+package com.pentakotavishu.mycustomproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MediaPlayer mediaPlayer= MediaPlayer.create(MainActivity.this,R.raw.christmas);
+
+        mediaPlayer.start();
+    }
+}
