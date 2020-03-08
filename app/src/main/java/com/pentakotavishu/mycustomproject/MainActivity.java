5d2e7702c350df59bc3248f69e02b6d5f2433528
vishpentak <vishpentak@gmail.com>
2020-03-08 01:35:23 -0500

    Lab06a Created the layout for the game and implemented a 30 second timer

--- app/src/main/java/com/pentakotavishu/mycustomproject/MainActivity.java
+++ app/src/main/java/com/pentakotavishu/mycustomproject/MainActivity.java
@@ -4,8 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView time;
+    Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +21,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         MediaPlayer mediaPlayer= MediaPlayer.create(MainActivity.this,R.raw.christmas);
 
+        time = findViewById(R.id.textView1);
+        start = findViewById(R.id.clickButton);
+
         mediaPlayer.start();
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Timer started!");
+
+                new CountDownTimer(30000, 1000) {
+
+                    public void onTick(long millisUntilFinished) {
+                        time.setText("Seconds Remaining: " + millisUntilFinished / 1000);
+                    }
+
+                    public void onFinish() {
+                        time.setText("done!");
+                    }
+                }.start();
+
+            }
+        });
     }
 }
